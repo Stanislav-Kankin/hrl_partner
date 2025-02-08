7143a87c82bdb633d5d0eb9bf674bd780c852619
Stnislav-Kankin <stanislav.kankin@yandex.ru>
2025-02-08 09:48:34 +0300

    сделал битру асинхронной

--- services/bitrix.py
+++ services/bitrix.py
@@ -1,4 +1,4 @@
-import requests
+import aiohttp
 import logging
 from typing import Optional, Dict, Any
 
@@ -9,34 +9,24 @@ class BitrixAPI:
     def __init__(self, webhook_base_url: str):
         self.webhook_base_url = webhook_base_url
 
-    def _call_method(
-            self, method: str, params: Optional[Dict] = None
-            ) -> Optional[Dict[str, Any]]:
-        """
-        Вызов метода Bitrix24 API.
-        """
+    async def _call_method(self, method: str, params: Optional[Dict] = None) -> Optional[Dict[str, Any]]:
         url = f"{self.webhook_base_url}/{method}"
-        try:
-            response = requests.post(url, json=params or {})
-            logger.info(f"Bitrix24 API Request URL: {url}")
-            logger.info(f"Bitrix24 API Request Params: {params}")
-            response.raise_for_status()
-            data = response.json()
+        logger.info(f"Request URL: {url}")
+        logger.info(f"Request Params: {params}")
 
-            if 'error' in data:
-                logger.error(f"Bitrix24 API Error: {data.get(
-                    'error_description', 'Unknown error')}")
+        async with aiohttp.ClientSession() as session:
+            try:
+                async with session.post(url, json=params or {}) as response:
+                    data = await response.json()
+                    if 'error' in data:
+                        logger.error(f"API Error: {data.get('error_description', 'Unknown error')}")
+                        return None
+                    return data
+            except aiohttp.ClientError as e:
+                logger.error(f"Request failed: {e}")
                 return None
 
-            return data
-        except requests.exceptions.RequestException as e:
-            logger.error(f"Request to Bitrix24 failed: {e}")
-            return None
-
-    def get_deal(self, deal_id: str) -> Optional[Dict]:
-        """
-        Получение информации о сделке по её ID.
-        """
-        response = self._call_method('crm.deal.get', {'id': deal_id})
-        logger.info(f"Bitrix24 API Response: {response}")
+    async def get_deal(self, deal_id: str) -> Optional[Dict]:
+        response = await self._call_method('crm.deal.get', {'id': deal_id})
+        logger.info(f"API Response: {response}")
         return response
